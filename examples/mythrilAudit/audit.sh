ab3d4211797dc640237a95d8cb1ba12ada4c5f0d
Víctor Nieves <vnievess@gmail.com>
2021-01-08 11:32:31 +0100

    renamed dir

--- examples/mythrilAudit/audit.sh
+++ examples/mythrilAudit/audit.sh
@@ -11,7 +11,7 @@ for d in ../SmartContracts/*/ ; do
 	for f in $d* ; do
 		contract=`echo ${f} | awk -F/ '{print $NF}'`
 		echo $contract
-		docker run -d --name $contract -v $(pwd)/../SmartContracts:/tmp mythril/myth -v 3 analyze --solv 0.4.23 --execution-timeout $TIMEOUT /tmp/identityManager/$contract -o jsonv2 > reports/audit_${contract}.json
+		docker run -d --name $contract -v $(pwd)/../alastriaSCs:/tmp mythril/myth -v 3 analyze --solv 0.4.23 --execution-timeout $TIMEOUT /tmp/identityManager/$contract -o jsonv2 > reports/audit_${contract}.json
 	done
 done
 exit 0
