088fcb66f9bb1ec5052c8e2e9cd1a33ab53c4b1d
Víctor Nieves <vnievess@gmail.com>
2021-01-09 18:36:07 +0100

    created getReports.sh

---
+++ examples/mythrilAudit/getReports.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+#
+# Usage: ./getReports.sh
+#
+#
+# This script collects the responses from audit.sh
+# and stores them in "reports/"
+#
+#
+# Author: Víctor Nieves Sánchez
+# email: vnievess@gmail.com
+#
+
+SCDIR=../alastriaSCs
+
+# Create reports dir if not exists
+mkdir -p reports
+# Timestamp
+timestamp=$(date "+%Y-%m-%d")
+
+for d in $SCDIR/*/ ; do
+	for f in $d* ; do
+		contract=`echo ${f} | awk -F/ '{print $NF}'`
+        docker logs $contract > reports/audit_${contract}_${timestamp}.txt
+    done
+done
+echo Reports obtained successfully
+exit 0
